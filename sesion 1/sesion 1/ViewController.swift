8360980d6cf1da8fd26ff1ad9046d0ce926a5e08
David Arroyo <chevy.arroyo@gmail.com>
2019-06-06 11:52:03 -0500

    Commit Inicial Sesion 1

---
+++ sesion 1/sesion 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sesion 1
+//
+//  Created by David Arroyo Sanchez on 6/6/19.
+//  Copyright © 2019 David Arroyo Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
