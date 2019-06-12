ac9d41a75be2b809be96c9c106a36c672a158dd9
David Arroyo Sanchez <desarroyo-mac@Davids-MacBook-Pro.local>
2019-06-12 00:18:41 -0500

    Interfaz Sesion 2

--- sesion 2/sesion 2/CocinaController.swift
+++ sesion 2/sesion 2/CocinaController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class CocinaController: UIViewController {
 
+    @IBOutlet weak var chkFreidora: UISwitch!
+    @IBOutlet weak var sldFreidoraTemp: UISlider!
+    @IBOutlet weak var lblTemp: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        chkFreidora.isOn = false
+        sldFreidoraTemp.isEnabled = chkFreidora.isOn
         
     }
     
     @IBAction func onFreidora(_ sender: Any) {
+        sldFreidoraTemp.isEnabled = chkFreidora.isOn
+    }
+    @IBAction func onSliderTemp(_ sender: Any) {
+        lblTemp.text = "Temp: " + (NSString(format: "%.2f", sldFreidoraTemp.value) as String) + "º"
     }
 }
